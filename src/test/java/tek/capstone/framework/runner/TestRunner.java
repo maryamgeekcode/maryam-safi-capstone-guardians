98789f237919b69eaf06f5e946d1c846b558c40c
maryam <maryamsafi69@yahoo.com>
2023-09-01 12:21:35 -0400

    updated the runner

--- src/test/java/tek/capstone/framework/runner/TestRunner.java
+++ src/test/java/tek/capstone/framework/runner/TestRunner.java
@@ -7,7 +7,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        tags = "@ReturnOrder",
+        tags = "@smoke",
         features = ("classpath:features"),
         glue="tek.capstone.framework",
         dryRun = false,
